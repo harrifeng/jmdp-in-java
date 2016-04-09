74af085b2a08bae9e7bea865a8a8a8abf87ef590
Harri Feng <harrifeng@gmail.com>
2016-04-09 17:21:13 +0800

    Great Blocking queue example

---
+++ src/main/java/org/hfeng/ch03/Main.java
@@ -0,0 +1,10 @@
+package org.hfeng.ch03;
+
+public class Main {
+    public static void main(String[] args) {
+        RequestQueue requestQueue = new RequestQueue();
+
+        new ClientThread(requestQueue, "Alice", 3141592L).start();
+        new ServerThread(requestQueue, "Bobby", 6535897L).start();
+    }
+}
