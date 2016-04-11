cb7ff3ae9ffeed93b69c717b4d35f2f08c96a8b2
Harri Feng <harrifeng@gmail.com>
2016-04-11 19:33:12 +0800

    reader can work with only readers

---
+++ src/main/java/org/hfeng/ch06/ex01/Main.java
@@ -0,0 +1,17 @@
+package org.hfeng.ch06.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        Data data = new Data(10);
+
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+
+        new WriterThread(data, "ABCDEFGHIJKLMNOPQRSTUVWXYZ").start();
+        new WriterThread(data, "abcdefghijklmnopqrstuvwxyz").start();
+    }
+}
