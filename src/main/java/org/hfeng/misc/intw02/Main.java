5078876513640ba5fd517dd3448ebc4d089e4275
Haoran Feng <haoran.feng@sap.com>
2016-04-21 16:41:03 +0800

    difference sequence control

--- src/main/java/org/hfeng/misc/intw02/Main.java
+++ src/main/java/org/hfeng/misc/intw02/Main.java
@@ -2,8 +2,8 @@ package org.hfeng.misc.intw02;
 
 public class Main {
     public static void main(String[] args) {
-
         Sequence sequence = new Sequence(1, 2, 3, 4);
+        // Sequence sequence = new Sequence(3, 4, 1, 2);
         new PrinterThread(1, sequence).start();
         new PrinterThread(2, sequence).start();
         new PrinterThread(3, sequence).start();
